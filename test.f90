018101666b3e316f5198f195108a5fa298b78320
Alexey Chernenkov <chernenkoval97@gmail.com>
2022-10-19 13:45:42 +0300

    second commit

---
+++ test.f90
@@ -0,0 +1,17 @@
+      module testmod
+         implicit none
+
+         integer b
+
+         b = 5
+      end testmod
+      
+      program testprogr
+         use testmod
+
+         implicit none
+
+         integer a
+
+         a=b
+      end testprogr
